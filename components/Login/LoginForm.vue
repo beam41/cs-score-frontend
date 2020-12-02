3b3ab8e50939aa7f146afe71d37dd257b0cbeb4d
Meehoi <beam_baimon@hotmail.com>
2020-12-02 15:32:42 +0700

    setup

---
+++ components/Login/LoginForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-form v-model="valid" @submit.prevent="login">
+    <v-card-title>Login</v-card-title>
+    <div class="mx-4">
+      <v-text-field
+        v-model="id"
+        type="text"
+        label="รหัส"
+        autocomplete="username"
+        required
+        outlined
+        dense
+        :disabled="loading"
+      />
+      <v-text-field
+        v-model="password"
+        type="password"
+        label="Password"
+        autocomplete="current-password"
+        required
+        outlined
+        dense
+        :disabled="loading"
+      />
+    </div>
+    <v-card-actions>
+      <v-btn
+        text
+        color="primary"
+        class="mr-4"
+        :loading="loading"
+        :disabled="!valid"
+        type="submit"
+      >
+        Login
+      </v-btn>
+    </v-card-actions>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    id: '',
+    password: '',
+  }),
+}
+</script>
+
+<style></style>
---
+++ components/Login/LoginHead.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-h1 title">
+      {{ title }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    title: 'CS Score',
+  }),
+}
+</script>
+
+<style lang="scss" scoped></style>
