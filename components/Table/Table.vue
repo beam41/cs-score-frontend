20a00718352ea5ced7f4a59a809ad0f6b3add9f6
kiissu <poolmean602@gmail.com>
2020-12-07 15:43:02 +0700

    TableFirststep

---
+++ components/Table/Table.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-simple-table fixed-header height="600px">
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">Group</th>
+          <th class="text-left">Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in desserts" :key="item.name">
+          <td>{{ item.group }}</td>
+          <td>{{ item.name }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      desserts: [
+        {
+          group: 1,
+          name: 'Pikachu Lnwza',
+        },
+        {
+          group: 2,
+          name: 'Cs LhorThae',
+        },
+        {
+          group: 3,
+          name: 'Minnie Mahapuy',
+        },
+        {
+          group: 4,
+          name: 'Gasha inmylife',
+        },
+        {
+          group: 5,
+          name: 'SiaO Huadoor',
+        },
+        {
+          group: 6,
+          name: 'LabKoi AroiHoH',
+        },
+        {
+          group: 7,
+          name: 'DinoSian',
+        },
+        {
+          group: 8,
+          name: 'SeedSad Roleplay',
+        },
+        {
+          group: 9,
+          name: 'Kitty Heebae',
+        },
+        {
+          group: 10,
+          name: 'HumAumarit',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
---
+++ components/Table/็Tablehead.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="title-wrapper d-flex align-center pt-8 pb-2"
+    height="calc(100vh-20vh)"
+  >
+    <h4 class="title text-h4">CS Score</h4>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.title-warpper {
+  align-content: flex-end;
+}
+</style>
